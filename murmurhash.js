151ed7ff4ff7f328d3397d1036a5c6779ff8c54a
Ian MacDonald <IanMacDonald@nanometrics.ca>
2019-07-02 15:33:29 -0400

    Removed bit shifts, which are error-prone in JavaScript.

--- murmurhash.js
+++ murmurhash.js
@@ -30,32 +30,32 @@
     while (l >= 4) {
       k =
         ((str[i] & 0xff)) |
-        ((str[++i] & 0xff) << 8) |
-        ((str[++i] & 0xff) << 16) |
-        ((str[++i] & 0xff) << 24);
+        ((str[++i] & 0xff) * (2 ** 8)) |
+        ((str[++i] & 0xff) * (2 ** 16)) |
+        ((str[++i] & 0xff) * (2 ** 24));
 
-      k = (((k & 0xffff) * 0x5bd1e995) + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16));
-      k ^= k >>> 24;
-      k = (((k & 0xffff) * 0x5bd1e995) + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+      k = (((k & 0xffff) * 0x5bd1e995) + (((Math.floor(k / (2 ** 16)) * 0x5bd1e995) & 0xffff) * (2 ** 16)));
+      k ^= k / (2 ** 24);
+      k = (((k & 0xffff) * 0x5bd1e995) + (((Math.floor(k / (2 ** 16)) * 0x5bd1e995) & 0xffff) * (2 ** 16)));
 
-    h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16)) ^ k;
+    h = (((h & 0xffff) * 0x5bd1e995) + (((Math.floor(h / (2 ** 16)) * 0x5bd1e995) & 0xffff) * (2 ** 16))) ^ k;
 
       l -= 4;
       ++i;
     }
 
     switch (l) {
-    case 3: h ^= (str[i + 2] & 0xff) << 16;
-    case 2: h ^= (str[i + 1] & 0xff) << 8;
+    case 3: h ^= (str[i + 2] & 0xff) * (2 ** 16);
+    case 2: h ^= (str[i + 1] & 0xff) * (2 ** 8);
     case 1: h ^= (str[i] & 0xff);
-            h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+            h = (((h & 0xffff) * 0x5bd1e995) + (((Math.floor(h / (2 ** 16)) * 0x5bd1e995) & 0xffff) * (2 ** 16)));
     }
 
-    h ^= h >>> 13;
-    h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16));
-    h ^= h >>> 15;
+    h ^= h / (2 ** 13);
+    h = (((h & 0xffff) * 0x5bd1e995) + (((Math.floor(h / (2 ** 16)) * 0x5bd1e995) & 0xffff) * (2 ** 16)));
+    h ^= h / (2 ** 15);
 
-    return h >>> 0;
+    return h / (2 ** 0);
   };
 
   /**
@@ -83,45 +83,45 @@
     i = 0;
 
     while (i < bytes) {
-        k1 =
+      k1 =
           ((key[i] & 0xff)) |
-          ((key[++i] & 0xff) << 8) |
-          ((key[++i] & 0xff) << 16) |
-          ((key[++i] & 0xff) << 24);
+          ((key[++i] & 0xff) * (2 ** 8)) |
+          ((key[++i] & 0xff) * (2 ** 16)) |
+          ((key[++i] & 0xff) * (2 ** 24));
       ++i;
 
-      k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
-      k1 = (k1 << 15) | (k1 >>> 17);
-      k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+      k1 = ((((k1 & 0xffff) * c1) + (((Math.floor(k1 / (2 ** 16)) * c1) & 0xffff) * (2 ** 16)))) & 0xffffffff;
+      k1 = (k1 * (2 ** 15)) | Math.floor(k1 / (2 ** 17));
+      k1 = ((((k1 & 0xffff) * c2) + (((Math.floor(k1 / (2 ** 16)) * c2) & 0xffff) * (2 ** 16)))) & 0xffffffff;
 
       h1 ^= k1;
-          h1 = (h1 << 13) | (h1 >>> 19);
-      h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
-      h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
+          h1 = (h1 * (2 ** 13)) | Math.floor(h1 / (2 ** 19));
+      h1b = ((((h1 & 0xffff) * 5) + (((Math.floor(h1 / (2 ** 16)) * 5) & 0xffff) * (2 ** 16)))) & 0xffffffff;
+      h1 = (((h1b & 0xffff) + 0x6b64) + (((Math.floor(h1b / (2 ** 16)) + 0xe654) & 0xffff) * (2 ** 16)));
     }
 
     k1 = 0;
 
     switch (remainder) {
-      case 3: k1 ^= (key[i + 2] & 0xff) << 16;
-      case 2: k1 ^= (key[i + 1] & 0xff) << 8;
+      case 3: k1 ^= (key[i + 2] & 0xff) * (2 ** 16);
+      case 2: k1 ^= (key[i + 1] & 0xff) * (2 ** 8);
       case 1: k1 ^= (key[i] & 0xff);
 
-      k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
-      k1 = (k1 << 15) | (k1 >>> 17);
-      k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
+      k1 = (((k1 & 0xffff) * c1) + (((Math.floor(k1 / (2 ** 16)) * c1) & 0xffff) * (2 ** 16))) & 0xffffffff;
+      k1 = (k1 * (2 ** 15)) | Math.floor(k1 / (2 ** 17));
+      k1 = (((k1 & 0xffff) * c2) + (((Math.floor(k1 / (2 ** 16)) * c2) & 0xffff) * (2 ** 16))) & 0xffffffff;
       h1 ^= k1;
     }
 
     h1 ^= key.length;
 
-    h1 ^= h1 >>> 16;
-    h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
-    h1 ^= h1 >>> 13;
-    h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
-    h1 ^= h1 >>> 16;
+    h1 ^= h1 / (2 ** 16);
+    h1 = (((h1 & 0xffff) * 0x85ebca6b) + (((Math.floor(h1 / (2 ** 16)) * 0x85ebca6b) & 0xffff) * (2 ** 16))) & 0xffffffff;
+    h1 ^= h1 / (2 ** 13);
+    h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + (((Math.floor(h1 / (2 ** 16)) * 0xc2b2ae35) & 0xffff) * (2 ** 16)))) & 0xffffffff;
+    h1 ^= h1 / (2 ** 16);
 
-    return h1 >>> 0;
+    return h1 / (2 ** 0);
   }
 
   var murmur = MurmurHashV3;
